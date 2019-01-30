d91cc64326ba70db7b6ab914a8c3cced52e7836f
Simone <simo.panzer89@gmail.com>
2019-01-30 23:35:13 +0100

    move the piece of code which runs the actual search to a separate routine

--- src/org/telegram/NewsFeederBot.java
+++ src/org/telegram/NewsFeederBot.java
@@ -20,58 +20,9 @@ public class NewsFeederBot extends TelegramLongPollingBot {
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			
 			myChatID = update.getMessage().getChatId();
+			//System.out.println("message received from chat ID " + String.valueOf(myChatID));
 			
-			for (int j = 0; j < NewsApiParams.KEYWORD_LIST.length; j++) {
-				String ch = NewsApiParams.KEYWORD_LIST[j];
-				
-				NewsApiQuery q = new NewsApiQuery();
-				q.setKeyword(ch);
-				System.out.println(q.getUrl());
-				
-				Vector<Article> v = q.execute();
-				int c = v.size();
-				if (c == 0) {
-					// send a message to announce no news were found.
-					SendMessage message = new SendMessage()
-							.setChatId(myChatID)
-							.setText("No news found for " + ch + ".");
-					try {
-						execute(message);
-					} catch (TelegramApiException e) {
-						e.printStackTrace();
-					}
-				}
-				else {
-					// send an intro message to announce news are coming
-					SendMessage messageIntro = new SendMessage()
-							.setChatId(myChatID)
-							.setText("Found news for " + ch + "!");
-					try {
-						execute(messageIntro);
-					} catch (TelegramApiException e) {
-						e.printStackTrace();
-					}
-					
-					// send the news
-					for (int i = 0; i < c; i++) {
-						Article a = v.get(i);
-
-						SendMessage message = new SendMessage()
-								.setChatId(myChatID)
-								.setText(a.getTitle() + "\n\n" + 
-										a.getDescription() + "\n" + 
-										a.getUrl());
-						try {
-							execute(message);
-						} catch (TelegramApiException e) {
-							e.printStackTrace();
-						}
-
-					}
-				}
-			}
-			
-			
+			runSearch();			
 		}
 		return;
 	}
@@ -79,6 +30,59 @@ public class NewsFeederBot extends TelegramLongPollingBot {
 	@Override
 	public String getBotToken() {
 		return BotConfig.BOT_TOKEN;
+	}
+	
+	private void runSearch() {
+		
+		for (int j = 0; j < NewsApiParams.KEYWORD_LIST.length; j++) {
+			String ch = NewsApiParams.KEYWORD_LIST[j];
+			
+			NewsApiQuery q = new NewsApiQuery();
+			q.setKeyword(ch);
+			System.out.println(q.getUrl());
+			
+			Vector<Article> v = q.execute();
+			int c = v.size();
+			if (c == 0) {
+				// send a message to announce no news were found.
+				SendMessage message = new SendMessage()
+						.setChatId(myChatID)
+						.setText("No news found for " + ch + ".");
+				try {
+					execute(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+			else {
+				// send an intro message to announce news are coming
+				SendMessage messageIntro = new SendMessage()
+						.setChatId(myChatID)
+						.setText("Found news for " + ch + "!");
+				try {
+					execute(messageIntro);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+				
+				// send the news
+				for (int i = 0; i < c; i++) {
+					Article a = v.get(i);
+
+					SendMessage message = new SendMessage()
+							.setChatId(myChatID)
+							.setText(a.getTitle() + "\n\n" + 
+									a.getDescription() + "\n" + 
+									a.getUrl());
+					try {
+						execute(message);
+					} catch (TelegramApiException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+		}
 	}
 
 }
